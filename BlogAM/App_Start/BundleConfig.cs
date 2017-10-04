c98cdd3a7e819b3eb5f33978ef764c888797e55c
giioohbernini <giovannibernini.ti@gmail.com>
2017-10-04 18:53:47 -0300

    new home layout closes #6

--- BlogAM/App_Start/BundleConfig.cs
+++ BlogAM/App_Start/BundleConfig.cs
@@ -8,36 +8,17 @@ namespace BlogAM
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
-
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
             bundles.Add(new ScriptBundle("~/bundles/loader").Include(
                 "~/Scripts/loader.js"
                 ));
 
-            bundles.Add(new ScriptBundle("~/bundles/eventpay-scripts").Include(
+            bundles.Add(new ScriptBundle("~/bundles/scripts").Include(
                 "~/Scripts/menu.js"
                 ));
 
-            bundles.Add(new ScriptBundle("~/Content/eventpay-styles").Include(
+            bundles.Add(new ScriptBundle("~/Content/styles").Include(
                 "~/Content/style.css"
                 ));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
-
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
         }
     }
 }
